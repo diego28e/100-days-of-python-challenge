4b92bae765a1172a4aec6343ed480baf885c9840
Diego E <diego28e@gmail.com>
2025-07-24 08:12:51 -0500

    day 1 out of 100 days of python challenge

---
+++ day1/task3.py
@@ -0,0 +1,8 @@
+#name = input("What's your name?")
+#print("Hi " + name)
+#print("This variable has " + str(len(name)))
+
+#print(len(input("What's your name?")))
+username=input("What's your name?")
+length=len(username)
+print("The user entered "+str(length) +" characters")
